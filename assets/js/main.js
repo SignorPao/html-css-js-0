0a54f0454027d1457db1ffdb7b7c3679ca479df0
Pavel Belousov <signorpao@gmail.com>
2023-12-20 13:27:46 +0300

    main section.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,29 @@
-/*=============== SHOW MENU ===============*/
+/*=============== SHOW/CLOSE MOBILE MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+// Menu show
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/* Menu hidden */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
+/*=============== REMOVE MENU MOBILE WHILE LINK CLICKED ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("showMenu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== DAY COUNTER FOR CHRISTMAS ===============*/
